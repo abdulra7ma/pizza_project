737fb3492deb25d0574f682fd79212250ebc1c07
Abdulrahman Dawoud <dawoud.abdulrahman.2004@gmail.com>
2023-05-01 23:14:45 +0600

    added pricing urls and updated the html

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from pizza.views import MainView, MenuView
+from pizza.views import MainView, MenuView, PricingView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("authentication.urls")),
     path("", MainView.as_view(), name="index"),
     path("menu/", MenuView.as_view(), name="menu"),
+    path("pricing/", PricingView.as_view(), name="pricing"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
